4034ccb41f6a3ecf0a4891898c9b250e88273e23
Shane Canon <scanon@lbl.gov>
2016-06-25 14:30:41 -0700

    Added myproxy/GSI support to auth.  Still hacky.

--- jupyterhub/auth.py
+++ jupyterhub/auth.py
@@ -20,10 +20,11 @@ from traitlets import Bool, Set, Unicode, Dict, Any, default, observe
 from .handlers.login import LoginHandler
 from .utils import url_path_join
 from .traitlets import Command
+from myproxy_login import myproxy_logon_py
 
 class Authenticator(LoggingConfigurable):
     """A class for authentication.
-    
+
     The primary API is one method, `authenticate`, a tornado coroutine
     for authenticating users.
     """
@@ -37,7 +38,7 @@ class Authenticator(LoggingConfigurable):
     ).tag(config=True)
     whitelist = Set(
         help="""Username whitelist.
-        
+
         Use this to restrict which users can login.
         If empty, allow any user to attempt login.
         """
@@ -54,10 +55,10 @@ class Authenticator(LoggingConfigurable):
         login services (e.g. 'GitHub').
         """
     )
-    
+
     username_pattern = Unicode(
         help="""Regular expression pattern for validating usernames.
-        
+
         If not defined: allow any username.
         """
     ).tag(config=True)
@@ -66,61 +67,61 @@ class Authenticator(LoggingConfigurable):
         if not change['new']:
             self.username_regex = None
         self.username_regex = re.compile(change['new'])
-    
+
     username_regex = Any()
-    
+
     def validate_username(self, username):
         """Validate a (normalized) username.
-        
+
         Return True if username is valid, False otherwise.
         """
         if not self.username_regex:
             return True
         return bool(self.username_regex.match(username))
-    
+
     username_map = Dict(
         help="""Dictionary mapping authenticator usernames to JupyterHub users.
-        
+
         Can be used to map OAuth service names to local users, for instance.
-        
+
         Used in normalize_username.
         """
     ).tag(config=True)
-    
+
     def normalize_username(self, username):
         """Normalize a username.
-        
+
         Override in subclasses if usernames should have some normalization.
         Default: cast to lowercase, lookup in username_map.
         """
         username = username.lower()
         username = self.username_map.get(username, username)
         return username
-    
+
     def check_whitelist(self, username):
         """Check a username against our whitelist.
-        
+
         Return True if username is allowed, False otherwise.
         No whitelist means any username should be allowed.
-        
+
         Names are normalized *before* being checked against the whitelist.
         """
         if not self.whitelist:
             # No whitelist means any name is allowed
             return True
         return username in self.whitelist
-    
+
     @gen.coroutine
     def get_authenticated_user(self, handler, data):
         """This is the outer API for authenticating a user.
-        
+
         This calls `authenticate`, which should be overridden in subclasses,
         normalizes the username if any normalization should be done,
         and then validates the name in the whitelist.
-        
+
         Subclasses should not need to override this method.
         The various stages can be overridden separately:
-        
+
         - authenticate turns formdata into a username
         - normalize_username normalizes the username
         - check_whitelist checks against the user whitelist
@@ -137,15 +138,15 @@ class Authenticator(LoggingConfigurable):
         else:
             self.log.warning("User %r not in whitelist.", username)
             return
-    
+
     @gen.coroutine
     def authenticate(self, handler, data):
         """Authenticate a user with login form data.
-        
+
         This must be a tornado gen.coroutine.
         It must return the username on successful authentication,
         and return None on failed authentication.
-        
+
         Checking the whitelist is handled separately by the caller.
 
         Args:
@@ -159,21 +160,21 @@ class Authenticator(LoggingConfigurable):
 
     def pre_spawn_start(self, user, spawner):
         """Hook called before spawning a user's server.
-        
+
         Can be used to do auth-related startup, e.g. opening PAM sessions.
         """
-    
+
     def post_spawn_stop(self, user, spawner):
         """Hook called after stopping a user container.
-        
+
         Can be used to do auth-related cleanup, e.g. closing PAM sessions.
         """
-    
+
     def add_user(self, user):
         """Add a new user
-        
+
         By default, this just adds the user to the whitelist.
-        
+
         Subclasses may do more extensive things,
         such as adding actual unix users,
         but they should call super to ensure the whitelist is updated.
@@ -185,57 +186,57 @@ class Authenticator(LoggingConfigurable):
             raise ValueError("Invalid username: %s" % user.name)
         if self.whitelist:
             self.whitelist.add(user.name)
-    
+
     def delete_user(self, user):
         """Triggered when a user is deleted.
-        
+
         Removes the user from the whitelist.
         Subclasses should call super to ensure the whitelist is updated.
-        
+
         Args:
             user (User): The User wrapper object
         """
         self.whitelist.discard(user.name)
-    
+
     def login_url(self, base_url):
         """Override to register a custom login handler
-        
+
         Generally used in combination with get_handlers.
-        
+
         Args:
             base_url (str): the base URL of the Hub (e.g. /hub/)
-        
+
         Returns:
             str: The login URL, e.g. '/hub/login'
-        
+
         """
         return url_path_join(base_url, 'login')
-    
+
     def logout_url(self, base_url):
         """Override to register a custom logout handler.
-        
+
         Generally used in combination with get_handlers.
-        
+
         Args:
             base_url (str): the base URL of the Hub (e.g. /hub/)
-        
+
         Returns:
             str: The logout URL, e.g. '/hub/logout'
         """
         return url_path_join(base_url, 'logout')
-    
+
     def get_handlers(self, app):
         """Return any custom handlers the authenticator needs to register
-        
+
         (e.g. for OAuth).
-        
+
         Args:
             app (JupyterHub Application):
                 the application object, in case it needs to be accessed for info.
         Returns:
             list: list of ``('/url', Handler)`` tuples passed to tornado.
                 The Hub prefix is added to any URLs.
-        
+
         """
         return [
             ('/login', LoginHandler),
@@ -246,7 +247,7 @@ class LocalAuthenticator(Authenticator):
 
     Checks for local users, and can attempt to create them if they exist.
     """
-    
+
     create_system_users = Bool(False,
         help="""If a user is added that doesn't exist on the system,
         should I try to create the system user?
@@ -254,26 +255,26 @@ class LocalAuthenticator(Authenticator):
     ).tag(config=True)
     add_user_cmd = Command(
         help="""The command to use for creating users as a list of strings.
-        
+
         For each element in the list, the string USERNAME will be replaced with
         the user's username. The username will also be appended as the final argument.
-        
+
         For Linux, the default value is:
-        
+
             ['adduser', '-q', '--gecos', '""', '--disabled-password']
-            
+
         To specify a custom home directory, set this to:
-        
+
             ['adduser', '-q', '--gecos', '""', '--home', '/customhome/USERNAME', '--disabled-password']
 
         This will run the command:
 
         adduser -q --gecos "" --home /customhome/river --disabled-password river
-        
+
         when the user 'river' is created.
         """
     ).tag(config=True)
-    
+
     @default('add_user_cmd')
     def _add_user_cmd_default(self):
         if sys.platform == 'darwin':
@@ -317,7 +318,7 @@ class LocalAuthenticator(Authenticator):
     @gen.coroutine
     def add_user(self, user):
         """Add a new user
-        
+
         If self.create_system_users, the user will attempt to be created.
         """
         user_exists = yield gen.maybe_future(self.system_user_exists(user))
@@ -326,9 +327,9 @@ class LocalAuthenticator(Authenticator):
                 yield gen.maybe_future(self.add_system_user(user))
             else:
                 raise KeyError("User %s does not exist." % user.name)
-        
+
         yield gen.maybe_future(super().add_user(user))
-    
+
     @staticmethod
     def system_user_exists(user):
         """Check if the user exists on the system"""
@@ -361,21 +362,21 @@ class PAMAuthenticator(LocalAuthenticator):
     ).tag(config=True)
     open_sessions = Bool(True,
         help="""Whether to open PAM sessions when spawners are started.
-        
+
         This may trigger things like mounting shared filsystems,
         loading credentials, etc. depending on system configuration,
         but it does not always work.
-        
+
         It can be disabled with::
-        
+
             c.PAMAuthenticator.open_sessions = False
         """
     ).tag(config=True)
-    
+
     @gen.coroutine
     def authenticate(self, handler, data):
         """Authenticate with PAM, and return the username if login is successful.
-    
+
         Return None otherwise.
         """
         username = data['username']
@@ -388,7 +389,7 @@ class PAMAuthenticator(LocalAuthenticator):
                 self.log.warning("PAM Authentication failed: %s", e)
         else:
             return username
-    
+
     def pre_spawn_start(self, user, spawner):
         """Open PAM session for user"""
         if not self.open_sessions:
@@ -399,7 +400,7 @@ class PAMAuthenticator(LocalAuthenticator):
             self.log.warning("Failed to open PAM session for %s: %s", user.name, e)
             self.log.warning("Disabling PAM sessions from now on.")
             self.open_sessions = False
-    
+
     def post_spawn_stop(self, user, spawner):
         """Close PAM session for user"""
         if not self.open_sessions:
@@ -410,4 +411,47 @@ class PAMAuthenticator(LocalAuthenticator):
             self.log.warning("Failed to close PAM session for %s: %s", user.name, e)
             self.log.warning("Disabling PAM sessions from now on.")
             self.open_sessions = False
-    
+
+
+class GSIAuthenticator(LocalAuthenticator):
+    """Authenticate local Linux/UNIX users with PAM"""
+    encoding = Unicode('utf8',
+        help="""The encoding to use for PAM"""
+    ).tag(config=True)
+    server = Unicode('server',
+        help="""The MY Proxy server to use for authentication."""
+    ).tag(config=True)
+    open_sessions = Bool(True,
+        help="""Whether to open PAM sessions when spawners are started.
+
+        This may trigger things like mounting shared filsystems,
+        loading credentials, etc. depending on system configuration,
+        but it does not always work.
+
+        It can be disabled with::
+
+            c.GSIAuthenticator.open_sessions = False
+        """
+    ).tag(config=True)
+
+    @gen.coroutine
+    def authenticate(self, handler, data):
+        """Authenticate with GSI, and return the proxy certificate if login is successful.
+
+        Return None otherwise.
+        """
+        username = data['username']
+        try:
+            #def myproxy_logon_py(hostname,username,passphrase,outfile,lifetime=43200,port=7512):
+
+            print(data)
+            resp=myproxy_logon_py(self.server,username, data['password'])
+            print(resp)
+        except:
+            if handler is not None:
+                self.log.warning("GSI Authentication failed (%s@%s):", username, handler.request.remote_ip)
+            else:
+                self.log.warning("GSI Authentication failed: ")
+            raise
+        else:
+            return username
